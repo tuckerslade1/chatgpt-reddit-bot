05f1f3be9f3d740a78d85b5e8eeabc000729b318
Tucker <tucker.kabbaz@gmail.com>
2023-10-31 01:40:32 -0400

    cache of replied posts

--- reddit_bot.py
+++ reddit_bot.py
@@ -1,20 +1,28 @@
 from dotenv import load_dotenv
 import os
 import praw
-from urllib.parse import quote_plus
 import openai
 import datetime
+import time
 
 load_dotenv()
+
+# todo: add models for more subreddits, move script to cloud to run 24/7, document process on personal website
 
 eli5_model = 'ft:gpt-3.5-turbo-0613:personal::8EpKrng8'
 eli5_system_message = 'You are a friendly, knowledgeable Reddit user explaining a concept in simple terms.'
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-min_post_score = 8 # minimum score (upvotes-downvotes) for a post to be replied to
-max_post_comments = 4 # maximum number of comments for a post to be replied to
-min_post_age = 60*5 # minimum age of post in seconds to be replied to (10 minutes)
+min_post_score = 5 # minimum score (upvotes-downvotes) for a post to be replied to
+max_post_comments = 6 # maximum number of comments for a post to be replied to
+min_post_age = 60*2 # minimum age of post in seconds to be replied to (2 minutes)
 max_post_age = 60*60*3 # maximum age of post in seconds to be replied to (3 hours)
+
+
+# keeping track of posts that have already been replied to
+with open('replied_posts.txt', 'a+') as f:
+    f.seek(0)  # Move file pointer to the beginning of the file
+    replied_posts = set(line.strip() for line in f)  # Read post IDs into a set
 
 def main():
 
@@ -31,8 +39,12 @@ def main():
     subreddit = reddit.subreddit('explainlikeimfive')
     for submission in subreddit.stream.submissions():
         # trying to select posts that are interesting, relatively new and not flooded with replies
-        if submission.score >= min_post_score and submission.num_comments <= max_post_comments and datetime.datetime.now().timestamp() - submission.created_utc >= min_post_age and datetime.datetime.now().timestamp() - submission.created_utc <= max_post_age:
+        if submission.id not in replied_posts and submission.score >= min_post_score and submission.num_comments <= max_post_comments and datetime.datetime.now().timestamp() - submission.created_utc >= min_post_age and datetime.datetime.now().timestamp() - submission.created_utc <= max_post_age:
             process_submission(submission)
+            replied_posts.add(submission.id)
+            with open('replied_posts.txt', 'a') as f:  # Append the post ID to the file
+                f.write(f"{submission.id}\n")
+            time.sleep(60*3) # at least 3 minutes between posts to avoid spamming
 
 
 def process_submission(submission):
