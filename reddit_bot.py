8c10fcf0262b2163d4d5290bbc34435459dd5df6
Tucker <tucker.kabbaz@gmail.com>
2023-11-03 11:18:43 -0400

    logging

--- reddit_bot.py
+++ reddit_bot.py
@@ -7,7 +7,7 @@ import time
 import pytz
 import random
 
-# todo: respond to being corrected, add models for more subreddits, move script to cloud to run 24/7, add conversation following and replying to replies, delete comments that get downvoted, document process on personal website
+# todo: logging comments, respond to being corrected, add models for more subreddits, move script to cloud to run 24/7, add conversation following and replying to replies, delete comments that get downvoted, document process on personal website
 
 load_dotenv()
 eastern = pytz.timezone('US/Eastern')
@@ -19,7 +19,7 @@ subs = {
         'system_message' : 'You are a friendly, knowledgeable Reddit user explaining a concept in simple terms. Please do not include hyperlinks or edit messages in your reply, or provide details about your life as if you are a real person.',
     },
     'relationship_advice' : {
-        'model' : os.getenv('RELATIONSHIPADVICE_MODEL'),
+        'model' : os.getenv('RELATIONSHIPADVICE_MODEL'), 
         'system_message' : 'You are a friendly Reddit user giving relationship advice. Please do not include hyperlinks or edit messages in your reply, or provide details about your life as if you are a real person.',
     },
     'changemyview' : {
@@ -40,6 +40,10 @@ with open('replied_posts.txt', 'a+') as f:
     f.seek(0)  # Move file pointer to the beginning of the file
     replied_posts = set(line.strip() for line in f)  # Read post IDs into a set
 
+with open('logs.txt', 'a+') as g:
+    g.seek(0)  # Move file pointer to the beginning of the file
+    logs = set(line.strip() for line in g)  # Read post IDs into a set
+
 def main():
 
     # api credentials
@@ -55,16 +59,16 @@ def main():
     subreddit = reddit.subreddit(current_sub)
     # listen for new posts and reply to them
     while True:
-        current_time = datetime.datetime.now(eastern).strftime("%I:%M%p")
-        print(f"{current_time}:   Sorting posts in {current_sub} by hot...")
+        current_time = datetime.datetime.now(eastern).strftime("%c")
+        log(f"{current_time}:   Sorting posts in {current_sub} by hot...")
         min_post_score = 30
         max_post_comments = 40
         min_post_age = 60*4 # seconds
         max_post_age = 60*60*24 # seconds
         for submission in subreddit.hot(limit=100):
             posted = False
-            current_time = datetime.datetime.now(eastern).strftime("%I:%M%p")
-            print(f"{current_time}:   Checking post: {submission.id} - Subreddit: {current_sub} Score: {submission.score} - Comments: {submission.num_comments} - Age: {datetime.datetime.now().timestamp() - submission.created_utc}")
+            current_time = datetime.datetime.now(eastern).strftime("%c")
+            log(f"{current_time}:   Checking post: {submission.id} - Subreddit: {current_sub} Score: {submission.score} - Comments: {submission.num_comments} - Age: {datetime.datetime.now().timestamp() - submission.created_utc}")
             # trying to select posts that are interesting, relatively new and not flooded with replies
             if submission.id not in replied_posts and submission.score >= min_post_score and submission.num_comments <= max_post_comments and datetime.datetime.now().timestamp() - submission.created_utc >= min_post_age and datetime.datetime.now().timestamp() - submission.created_utc <= max_post_age:
                 process_submission(submission, current_sub)
@@ -74,15 +78,15 @@ def main():
                 break
 
         # try sorting by new if no suitable posts found in hot
-        current_time = datetime.datetime.now(eastern).strftime("%I:%M%p")
-        print(f"{current_time}:   No suitable posts found in hot. Sorting by new...")
+        current_time = datetime.datetime.now(eastern).strftime("%c")
+        log(f"{current_time}:   No suitable posts found in hot. Sorting by new...")
         min_post_score = 5
         max_post_comments = 15
         min_post_age = 60*4 # seconds
         max_post_age = 60*60*24 # seconds
         for submission in subreddit.new(limit=100):
-            current_time = datetime.datetime.now(eastern).strftime("%I:%M%p")
-            print(f"{current_time}:   Checking post: {submission.id} - Subreddit: {current_sub} - Score: {submission.score} - Comments: {submission.num_comments} - Age: {datetime.datetime.now().timestamp() - submission.created_utc}")
+            current_time = datetime.datetime.now(eastern).strftime("%c")
+            log(f"{current_time}:   Checking post: {submission.id} - Subreddit: {current_sub} - Score: {submission.score} - Comments: {submission.num_comments} - Age: {datetime.datetime.now().timestamp() - submission.created_utc}")
             # trying to select posts that are interesting, relatively new and not flooded with replies
             if submission.id not in replied_posts and submission.score >= min_post_score and submission.num_comments <= max_post_comments and datetime.datetime.now().timestamp() - submission.created_utc >= min_post_age and datetime.datetime.now().timestamp() - submission.created_utc <= max_post_age:
                 process_submission(submission, current_sub)
@@ -91,7 +95,7 @@ def main():
         current_sub = list(subs.keys())[random.randint(0, 1)]
         subreddit = reddit.subreddit(current_sub)
         sleep_time = random.randint(180, 361)
-        print(f"{current_time}:   Next sub: {current_sub} - Sleeping for {sleep_time} seconds...\n")
+        log(f"{current_time}:   Next sub: {current_sub} - Sleeping for {sleep_time} seconds...\n")
         time.sleep(sleep_time)
 
 
@@ -101,10 +105,10 @@ def process_submission(submission, current_sub):
     model = subs[current_sub]['model']
     system_message = subs[current_sub]['system_message']
     reply_text = generate_reply(prompt, model, system_message)
-    current_time = datetime.datetime.now(eastern).strftime("%I:%M%p")
+    current_time = datetime.datetime.now(eastern).strftime("%c")
     if reply_text:
-        print(f"{current_time}:   Replying to:   {submission.id} - {submission.title}\n")
-        print(f"{current_time}:  \"{reply_text}\"\n")
+        log(f"{current_time}:   Replying to:   {submission.id} - {submission.title}\n")
+        log(f"{current_time}:  \"{reply_text}\"\n")
         submission.reply(reply_text)
         replied_posts.add(submission.id)
         with open('replied_posts.txt', 'a') as f:  # Append the post ID to the file
@@ -133,14 +137,19 @@ def generate_reply(prompt, model_id, system_instruction=None, max_tokens=512, ma
         # check if the reply is complete by looking for end of sentence marker (only somewhat accurate)
         if assistant_reply[-1] in ['.', '?', '!'] and len(assistant_reply) >= min_length:
             return assistant_reply
-    current_time = datetime.datetime.now(eastern).strftime("%I:%M%p")
+    current_time = datetime.datetime.now(eastern).strftime("%c")
     if len(assistant_reply) < min_length:
-        print('Failed to generate reply of minimum length.')
+        log('Failed to generate reply of minimum length.')
     elif assistant_reply[-1] not in ['.', '?', '!']:
-        print(f'{current_time}:   Error: Failed to generate reply within token limit.')
+        log(f'{current_time}:   Error: Failed to generate reply within token limit.')
     else:
-        print(f'{current_time}:   Error: Failed to generate reply for unknown reason. Last attempt: \n\"{assistant_reply}\"')
+        log(f'{current_time}:   Error: Failed to generate reply for unknown reason. Last attempt: \n\"{assistant_reply}\"')
     return False
+
+def log(message):
+    print(message)
+    with open('logs.txt', 'a') as f:  # Append the post ID to the file
+        f.write(f'{message}\n')
 
 
 if __name__ == '__main__':
